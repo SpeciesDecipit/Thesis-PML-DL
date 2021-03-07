9a4801f3927ddda0666709bc4bd4f2c324e95fb5
Vladik <qvviko@gmail.com>
2021-03-07 16:49:45 +0300

    Tuned model

--- model/train.py
+++ model/train.py
@@ -16,6 +16,7 @@ from data.utils import get_antipattern_dict, get_labels, get_embeddings
 from model import PredictionHead
 from utils.roc import get_accuracy_multiclass
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 warnings.filterwarnings('ignore')
 
@@ -23,8 +24,6 @@ warnings.filterwarnings('ignore')
 def train_step_with_roc(model, train_loader, loss_func, optimizer, device, thresholds):
     model.train()
     losses_train = []
-    correct = 0
-    total = 0
     pbar = tqdm(train_loader)
     preds, lbls = [], []
     for X, y in pbar:
@@ -38,18 +37,9 @@ def train_step_with_roc(model, train_loader, loss_func, optimizer, device, thres
         loss = loss_func(out, y)
         loss.backward()
         optimizer.step()
-
-        # make predictions
-        y = y.reshape(-1).cpu().numpy()
-        acc_preds = (out > 0.5).reshape(-1).detach().cpu().numpy().astype(int)
-
         # compute metrics
-        cur_correct = np.sum(acc_preds == y)
-        correct += cur_correct
-        total += len(y)
-
         losses_train.append(loss.item())
-        _, acc = get_accuracy_multiclass(np.vstack(preds), np.vstack(lbls))
+        _, acc = get_accuracy_multiclass(np.vstack(preds), np.vstack(lbls), thresholds)
         pbar.set_description(
             f'TRAIN. Mean Loss [{np.mean(losses_train):.2f}]. Accuracy [{acc:.2f}]')
     return losses_train
@@ -72,7 +62,7 @@ def train_step(model, train_loader, loss_func, optimizer, device):
 
         # make predictions
         y = y.reshape(-1).cpu().numpy()
-        preds = (out > 0.5).reshape(-1).detach().cpu().numpy().astype(int)
+        preds = (torch.sigmoid(out) > 0.5).reshape(-1).detach().cpu().numpy().astype(int)
 
         # compute metrics
         cur_correct = np.sum(preds == y)
@@ -91,16 +81,20 @@ def valid_step(model, valid_loader, loss_func, device):
     total = 0
     model.eval()
     pbar = tqdm(valid_loader)
+    total_preds = []
+    total_lbls = []
     for X, y in pbar:
         X, y = X.to(device), y.to(device)
         out = model(X)
 
         loss = loss_func(out, y)
         # make predictions
-        y = y.reshape(-1).cpu().numpy()
-        preds = (out > 0.5).reshape(-1).detach().cpu().numpy().astype(int)
+        preds = (torch.sigmoid(out) > 0.5).reshape(-1).detach().cpu().numpy().astype(int)
+        total_preds.append((torch.sigmoid(out) > 0.5).detach().cpu().numpy().astype(int))
+        total_lbls.append(y.cpu().numpy())
 
         # compute metrics
+        y = y.reshape(-1).cpu().numpy()
         cur_correct = np.sum(preds == y)
         correct += cur_correct
         total += len(y)
@@ -108,13 +102,12 @@ def valid_step(model, valid_loader, loss_func, device):
         losses_valid.append(loss.item())
         pbar.set_description(
             f'VALID. Mean Loss [{np.mean(losses_valid):.2f}]. Mean Accuracy [{correct / total:.2f}]')
+    print(classification_report(np.vstack(total_lbls), np.vstack(total_preds)))
     return losses_valid
 
 
 def valid_step_with_roc(model, train_loader, loss_func, device):
     losses_valid = []
-    correct = 0
-    total = 0
     model.eval()
     pbar = tqdm(train_loader)
     preds, lbls = [], []
@@ -126,20 +119,14 @@ def valid_step_with_roc(model, train_loader, loss_func, device):
         preds.append(torch.sigmoid(out).cpu().detach().numpy())
         loss = loss_func(out, y)
 
-        # make predictions
-        y = y.reshape(-1).cpu().numpy()
-        acc_preds = (out > 0.5).reshape(-1).detach().cpu().numpy().astype(int)
-
         # compute metrics
-        cur_correct = np.sum(acc_preds == y)
-        correct += cur_correct
-        total += len(y)
-
         losses_valid.append(loss.item())
         _, acc = get_accuracy_multiclass(np.vstack(preds), np.vstack(lbls))
         pbar.set_description(
             f'VALID. Mean Loss [{np.mean(losses_valid):.2f}]. Accuracy [{acc:.2f}]')
-    thresholds, _ = get_accuracy_multiclass(np.vstack(preds), np.vstack(lbls))
+    preds = np.vstack(preds)
+    thresholds, _ = get_accuracy_multiclass(preds, np.vstack(lbls))
+    print(classification_report(np.vstack(lbls), preds))
     return losses_valid, thresholds
 
 
@@ -154,8 +141,8 @@ embedding_dim = 384
 conv_out_dim = 128
 lstm_hidden = 128
 n_labels = len(name2label)
-lr = 1e-3
-epochs = 3
+lr = 1e-4
+epochs = 5
 batch_size = 16
 out_path = 'state_dict'
 
@@ -173,6 +160,8 @@ thresholds = np.repeat(0.5, n_labels)
 valid_means = []
 train_means = []
 for epoch in range(epochs):
+    # losses_train = train_step_with_roc(model, train_loader, loss_func, optimizer, device, thresholds)
+    # losses_valid, thresholds  = valid_step_with_roc(model, valid_loader, loss_func, device)
     losses_train = train_step(model, train_loader, loss_func, optimizer, device)
     losses_valid = valid_step(model, valid_loader, loss_func, device)
     valid_means.append(np.mean(losses_valid))
@@ -183,3 +172,4 @@ plt.plot(valid_means)
 plt.plot(train_means)
 plt.legend(["Validation loss", "Training loss"], loc="upper left")
 plt.show()
+print(thresholds)
